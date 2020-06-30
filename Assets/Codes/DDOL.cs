ddbce5400855da49874f5c42396685c661c36b26
gabrielolsimoes <gabriel.olsimoes@gmail.com>
2020-06-30 20:16:50 -0300

    Novo level e Criação das shrines

---
+++ Assets/Codes/DDOL.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDOL : MonoBehaviour
+{
+
+    public GameObject[] ShrineDoorLocations;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //pega o nome do da shrine, spawn point etc e da spawn no player no local marcado aqui.
+            //então por exemplo, desde o iniciio do gameplay na scene 01 a gente pega o spawn point e guarda no ddol.
+            //carrega a segunda scene e o spawn point ja vai ser na frente do krl da shrine correspondente.
+            //mata o boss e entra na shrine de novo.
+            //dessa vez vou ter numa variável SHRINE LOCATION ENTERED o novo spawn point do player.
+            //então a gente joga a localização do player spawn point nessa variável
+            
+        //TALVEZ o ddol deva ser aplicado no player tb. Daí coloca num onload pra o player spawnar no ponto desejado JA ESTANDO COM A ARMA EM MÃOS.
+        }
+    }
+}
---
+++ Assets/SauronsGaze.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauronsGaze : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockOnTheEnemy();
+    }
+
+    void LockOnTheEnemy()
+    {
+        Vector3 relativePos = (player.transform.position - transform.position);
+
+        Quaternion LookAtRotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion LookAtRotationOnly_Y = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, /*LookAtRotationOnly_Y*/ LookAtRotation, 3);
+    }
+}
